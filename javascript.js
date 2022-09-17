288db82024a301b23bc6a01d2efedff7d2c1d197
jordan <jordanjgoff@gmail.com>
2022-09-16 18:35:00 -0600

    Declare the playRound function
    The playRound function compares the player's choice versus the
    computer's choice. The "Rock", "Paper", and "Scissor" strings were
    also changed to lowercase to make sure the inputs from the user are
    case insensitive.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,40 @@
 // Randomly return "Rock", "Paper", or "Scissors".
 function getComputerChoice() {
   // Get a random integer between 0 and 2.
+  // Math.random() is the interval [0, 1).
   choice = Math.floor(3 * Math.random());
 
   // Return the computer's choice depending on the random integer.
   switch (choice) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     default:
-      return "Scissors";
+      return "scissors";
+  }
+}
+
+
+
+// Play a round and return a string that dictates the winner.
+function playRound(playerSelection, computerSelection) {
+  // Make the playerSelection case insensitive.
+  playerSelection = playerSelection.toLowerCase();
+  
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper";
+  } else {
+    return "Tie!";
   }
 }
